f752c7908886e296f123824f94c85e710f47f094
jher235 <tim668666@gmail.com>
2023-07-16 15:43:47 +0900

    게시판 불러오기

---
+++ src/routes/CreatePost.js
@@ -0,0 +1,156 @@
+import React, { useState,useEffect } from "react";
+import axios from "axios";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faLock } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {Link, useNavigate} from "react-router-dom"
+import "../css/register.css";
+
+
+function CreatePost(props){
+    
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [password2, setPassword2] = useState("");
+    const [email, setEmail] = useState("");
+    const [nickname, setNickname] = useState("");
+    const navigate = useNavigate();
+
+
+  
+    useEffect(()=>{
+      if(localStorage.getItem("token") != null){
+          localStorage.clear();
+      }
+  },[])
+
+    
+  const handleChange = (event) => {
+    const target = event.target;
+    if (target.name === "username") {
+      setUsername(target.value);
+    } else if (target.name === "password") {
+      setPassword(target.value);
+    }  else if (target.name === "password2"){
+        setPassword2(target.value);
+    }   else if (target.name === "email"){
+        setEmail(target.value);
+    } else if (target.name === "nickname"){
+        setNickname(target.value);
+    }
+        
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("[Register.js] handleSubmit");
+    axios
+      .post("http://localhost:8000/users/register/", {
+        username: username,
+        password: password,
+        password2: password2,
+        email : email,
+      })
+      .then((response) => {
+        if (response.status < 300) {
+          console.log("[Register.js] Call props.doRegister");
+          if (props.doLogin) {
+            props.doLogin();
+          }
+          navigate("/login")
+        }
+      })
+      .catch((error)=>{
+        console.error("Error:",error);
+        
+        console.log(error.response);
+        if(error.response.data){
+        const errorData = error.response.data
+        for(const field in errorData){
+            const errorMessage = errorData[field]
+            alert(`${field}:${errorMessage[0]}`)
+        }}
+      })
+    
+  };
+
+
+
+
+
+
+
+
+  return (
+    <div className="body">
+    <main className="form-signup ">
+    <form onSubmit={handleSubmit}>
+      
+      <h1 className=" mb-3 fw-normal logintext">Create<br/>account</h1>
+      <div className="form-floating">
+        <input
+          type="text"
+          className="form-control"
+          name="username"
+          id="floatingInput"
+          placeholder="Id"
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="floatingInput">Id</label>
+      </div>
+      <div className="form-floating">
+        <input
+          type="password"
+          className="form-control"
+          name="password"
+          id="floatingPassword"
+          placeholder="Password"
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="floatingPassword">Password</label>
+      </div>
+
+      <div className="form-floating">
+        <input
+          type="password"
+          className="form-control"
+          name="password2"
+          id="floatingPassword2"
+          placeholder="Password2"
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="floatingPassword2">Password2</label>
+      </div>
+      <div className="form-floating">
+        <input
+          type="text"
+          className="form-control"
+          name="email"
+          id="floatingEmail"
+          placeholder="Email"
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="floatingEmail">Email</label>
+      </div>
+      
+      <div className="checkbox mb-3">
+      </div>
+      <button className="w-100 btn btn-lg btn-light" type="submit">
+        Sign up
+      </button>
+      <div className="pluslink ">
+        <Link to="/" className="homelink">Home</Link>
+      </div>
+      
+    </form>
+  </main>
+  </div>
+  );
+}
+
+export default CreatePost;
--- src/routes/Posts.js
+++ src/routes/Posts.js
@@ -1,4 +1,4 @@
-import {React, useEffect, useState} from "react"
+import {Fragment, React, useEffect, useState} from "react"
 import "../css/posts.css"
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { useNavigate,Link } from "react-router-dom";
@@ -9,7 +9,7 @@ import axios from "axios";
 
 function Posts(){
   const navigate = useNavigate()
-  const [post,setPost] = useState("")
+  const [post,setPost] = useState({results:[]})
 
 
   const handleLogout=()=>{
@@ -26,77 +26,84 @@ function Posts(){
     })
     .then(response=>{
       if(response.status<300){
-        setPost(response.data)
-
+        setPost(response.data);
+        console.log(post);
       }
     })
   },[])
 
 return(
  <div>
- <div className="">   
- <nav class="content-left navbar navbar-expand-lg bg-body-tertiary ">
-  <div class="container-fluid">
-    <Link to="/" class="nav-link ms-4 me-3"><a class="nav-link titlePlus ms-4" ><h1>명지도</h1></a></Link>
+ <div className="bgb">   
+ <nav className="content-left navbar navbar-expand-lg bg-body-tertiary ">
+  <div className="container-fluid">
+    <Link to="/" className="nav-link ms-4 me-3"><a className="nav-link titlePlus ms-4" ><h1>명지도</h1></a></Link>
 
     
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav mx-auto">
+    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul className="navbar-nav mx-auto">
         {/* <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li> */}
 
-        <li class="nav-item">
-          <a class="nav-link title">
+        <li className="nav-item">
+          <a className="nav-link title">
             게시판
           </a>
         </li>
       </ul>
       <div>
-      <ul class=" navbar-nav ms-auto login-margin ">
+      <ul className=" navbar-nav ms-auto login-margin ">
         
           {/* <a class="nav-link btn btn-primary" data-bs-toggle="modal" data-bs-target="#loginModal" href="#">Log In <FontAwesomeIcon icon={faArrowRightToBracket}/></a>
            */}
            {localStorage.getItem("token") ? 
-             <li class="nav-item dropdown ">
-                <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+             <li className="nav-item dropdown ">
+                <a className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   {localStorage.getItem("username")}
                 </a>
-              <ul class="dropdown-menu ">
-                  <li><a class="dropdown-item" href="#">Profile</a></li>
-                  <li><a class="dropdown-item" onClick={handleLogout}>Log Out</a></li>
+              <ul className="dropdown-menu ">
+                  <li><a className="dropdown-item" href="#">Profile</a></li>
+                  <li><a className="dropdown-item" onClick={handleLogout}>Log Out</a></li>
               </ul>
             </li>  : 
-             <li class="nav-item"> <Link to="/login" class="nav-link btn btn-light" >Log In <FontAwesomeIcon icon={faArrowRightToBracket}/></Link></li>
+             <li className="nav-item"> <Link to="/login" className="nav-link btn btn-light" >Log In <FontAwesomeIcon icon={faArrowRightToBracket}/></Link></li>
            } 
         </ul>
         </div>
       </div>
     </div>
 </nav>
-   
-  <hr className="featurette-divider"/>
-   
 
-    
+  
+ { post.results.map((value)=>(
+
+    <Fragment key={value.pk}>
+  <hr className="featurette-divider"/>
+  
     <div className="row post">
       <div className="col-md-5 order-md-2">
-        <h4 className="post-title">Oh yeah, it’s that good. <span className="text-muted">See for yourself.</span></h4>
-        <p className="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+        <h4 className="post-title">{value.title}</h4>
+        <p className="lead post-body">{value.body}</p>
       </div>
       <div className="col-md-3 order-md-1">
         <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid ms-5" width="150" height="150" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: no-image" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text font-size="50%" x="50%" y="50%" fill="#aaa" dy=".3em">No-Image</text></svg>
 
       </div>
-      <div class="col-md-4 order-md-2 post-sub">
-        <a class="post-author">by</a><br/>
-        <a class="post-date">dd</a>
+      <div className="col-md-4 order-md-2 post-sub">
+        <a className="post-author">by 
+        <svg className="profile-image  img-fluid ms-3" width="40" height="40" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: no-image" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/>{value.profile.image? <image>{value.profile.image}</image>:<text font-size="50%" x="50%" y="50%" fill="#aaa" dy=".3em">No-Image</text>}</svg>
+          {value.profile.nickname}
+          </a><br/>
+        <a className="post-date">{value.published_date}</a>
       </div>
     </div>
-
+    </Fragment>
+   
+))}
   
 
  
