340868fa0248908bdb54ecc19cd487b4ec7c96be
jher235 <tim668666@gmail.com>
2023-07-20 14:55:33 +0900

    포스트 디테일 페이지 작업중

--- src/routes/PostDetail.js
+++ src/routes/PostDetail.js
@@ -19,20 +19,8 @@ function PostDetail(){
     const postid = useParams();
   
   
-    const handleLogout=()=>{
-      localStorage.clear();
-      navigate('/post_list');
-    }
   
-    const older=()=>{
-      if(next !== null)
-      setPage(page+1);
-    }
-    const later=()=>{
-      if(previous !== null){
-      setPage(page-1);
-      }
-    }
+
   
     useEffect(()=>{
       console.log(postid.postId);
@@ -44,9 +32,6 @@ function PostDetail(){
         if(response.status<300){
           setPost(response.data);
           console.log(response);
-          console.log(post);
-         
-          
         }
       })
       .catch((error)=>{
@@ -54,8 +39,14 @@ function PostDetail(){
           setReason(error.message);
           console.log(reason);
       })
-    },[page])
+    },[])
   
+    useEffect(() => {
+      // post 상태 업데이트 후 출력
+      console.log("2  " + JSON.stringify(post));
+  }, [post]);
+
+
   return(
    <div>
    <div className="bgb">   
@@ -63,41 +54,57 @@ function PostDetail(){
   
     <Link to="/post_create" className="btn btn-light createbt"><FontAwesomeIcon icon={faPen}/> 수정하기</Link>
     
-   {/* {post.results.length!=0 ? post.results.map((value)=>(
+   {post !=={} ? 
   
-      <Fragment key={value.pk}>
-    <hr className="featurette-divider"/>
-    
-      <div className="row post">
-        <div className="col-md-5 order-md-2">
-          <h4 className="post-title">{value.title}</h4>
-          <p className="lead post-body">{value.body}</p>
+
+      <div className=" post">
+        <div className="col-md-8 ">
+          <div className="post-head ">
+            <h4 className="post-title ">{post.title}</h4>
+            
+                <div className="post-author">
+                <div className="post-date ms-3"> {new Date(post.published_date).toLocaleString()}</div>
+                 <div>
+                    by 
+                    {post.profile?
+                    <>
+                        <img src={post.profile.image} className="profile-image  img-fluid ms-3 me-3" width="40" height="40"/>
+                        {post.profile.nickname}
+                    </>
+                    :<a>Loading...</a>
+                    }
+                  </div>
+                </div>
+            
+          </div>
+          <hr className="featurette-divider"/>
+          {post.image?
+          <>
+          <img>{post.image}</img>
+          <hr className="featurette-divider"/> 
+          </>
+          : <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid ms-5" width="150" height="150" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: no-image" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text font-size="50%" x="50%" y="50%" fill="#aaa" dy=".3em">No-Image</text></svg>
+          }
+          <p className="lead post-body">{post.body}</p>
         </div>
-        <div className="col-md-3 order-md-1">
-          <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid ms-5" width="150" height="150" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: no-image" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text font-size="50%" x="50%" y="50%" fill="#aaa" dy=".3em">No-Image</text></svg>
+        <div className="col-md-3 ">
+          
   
         </div>
         <div className="col-md-4 order-md-2 post-sub">
-          <a className="post-author">by 
-          <svg className="profile-image  img-fluid ms-3 me-3" width="40" height="40" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: no-image" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/>{value.profile.image? <image>{value.profile.image}</image>:<text font-size="50%" x="50%" y="50%" fill="#aaa" dy=".3em">No-Image</text>}</svg>
-              {value.profile.nickname}
-            </a>
+          
             <br/>
             <br/>
-          <a className="post-date"> {new Date(value.published_date).toLocaleString()}</a>
+          
         </div>
       </div>
-      </Fragment>
+      
      
-  )):reason?
+  :reason?
       <div className="loading-container"><h1 className="loading"> {reason}</h1></div>
       :<div className="loading-container"><h1 className="loading">Loading...</h1></div>
       }
-          <div className="pagebt">
-          <button className="btn btn-light" onClick={later}>l</button>
-          <block className="btn btn-secondary pgblock">{page}</block>
-           <button className="btn btn-light" onClick={older}>r</button>
-        </div> */}
+         
     </div>
     </div>
         
